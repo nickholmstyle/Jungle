29c534bfea9329ff30645d758eb7144b86555b10
nickholmstyle <nick416holm@protonmail.com>
2022-10-24 21:01:40 +0000

    home page to product a go clicking name

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -4,10 +4,17 @@ describe('Home page', () => {
     cy.get(".products article").should("have.length", 2);
   });
 
-  it("clicking on a product's image from the homepage takes the user to the product details page", () => {
+  it("clicking on a product's image from the homepage takes the user to the product page", () => {
     cy.get("img")
       .first()
       .click()
+      .visit('/products/2')
+  });
+
+  it("clicking on a product's name from the homepage takes the user to the product page", () => {
+    cy.get("h1")
+      .contains("Scented Blade")
+      .click({ force:true })
       .visit('/products/2')
   });
 

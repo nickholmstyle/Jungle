8380647f6c8b873c220fa9e3a55c0e8939c82da3
nickholmstyle <nick416holm@protonmail.com>
2022-10-24 20:30:58 +0000

    cypress testing for 2 products on homepage

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,13 @@
+describe('home page', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  });
+
+  it('There are products on the page', () => {
+    cy.get('.products article').should('be.visible');
+  });
+
+  it('There are 2 products on the page', () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+});

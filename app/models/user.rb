aa4aeaaefa0601d009ede9f462b211bb0ecbe79e
nickholmstyle <nick416holm@protonmail.com>
2022-10-25 00:43:30 +0000

    added fields to the user model to validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  validates :email, presence: true, confirmation: { case_sensitive: false }, uniqueness: true
+
+  validates :password, presence: true, confirmation: true, length: { minimum: 3 }
+  validates :password_confirmation, presence: true
+
+
 end

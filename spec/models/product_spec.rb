72ebf0d61a70312d6c286e49838b4a114e968902
nickholmstyle <nick416holm@protonmail.com>
2022-10-25 00:00:52 +0000

    all product validations pass

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
+#loops through the required fields and validates they are true by their presence.
+
 RSpec.describe Product, type: :model do
   describe 'Validations' do
     # validation tests/examples here
     before(:each) do
       @product = Product.new do |p|
         p.name = "Leafy"
-        p.price = 30
+        p.price_cents = 30
         p.quantity = 5
         p.category = Category.new
       end
@@ -15,10 +17,30 @@ RSpec.describe Product, type: :model do
     it 'is valid with all four fields' do
       expect(@product).to be_valid
     end
+
+    it "is not valid without a name" do
+      @product.name = nil
+      expect(@product).to_not be_valid
+    end
+
+    it "is not valid without a price" do
+      @product.price_cents = nil
+      expect(@product).to_not be_valid
+    end
+
+    it "is not valid without a quantity" do
+      @product.quantity = nil
+      expect(@product).to_not be_valid
+    end
+
+    it "is not valid without a category" do
+      @product.category = nil
+      expect(@product).to_not be_valid
+    end
   end  
 end
 
 # validates :name, presence: true
-# validates :price, presence: true
+# validates :price, presence: true ## needs to be price_cents
 # validates :quantity, presence: true
 # validates :category, presence: true
